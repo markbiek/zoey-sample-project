6cd3fcfa7cc32255f4ff3a37b5b2f38e65eefe59
Mark Biek <support@janustech.net>
2021-12-22 11:25:51 -0500

    Initialize illuminate/database

--- index.php
+++ index.php
@@ -1,5 +1,25 @@
 <?php
 require_once __DIR__ . '/vendor/autoload.php';
 
+use Illuminate\Database\Capsule\Manager as DB;
+
+// Bootup our environment config
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
+
+// Bootup the database connection
+$db = new DB();
+$db->addConnection([
+	'driver' => 'mysql',
+	'host' => $_ENV['DB_HOST'],
+	'database' => $_ENV['DB_NAME'],
+	'username' => $_ENV['DB_USER'],
+	'password' => $_ENV['DB_PASS'],
+	'charset' => 'utf8',
+	'collation' => 'utf8_unicode_ci',
+	'prefix' => '',
+]);
+$db->setAsGlobal();
+$db->bootEloquent();
+
+echo '<hr />done';
