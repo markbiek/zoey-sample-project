78679ddbf7ef2ae611d3702cedbb7e638ad8d16a
Mark Biek <support@janustech.net>
2021-12-22 11:44:29 -0500

    Move setup code to bootstrap.php
    This tidies up index.php and makes it easier to bootstrap our app from tests or Tinkerwell

--- index.php
+++ index.php
@@ -1,25 +1,7 @@
 <?php
-require_once __DIR__ . '/vendor/autoload.php';
+ini_set('display_errors', true);
+error_reporting(E_ALL);
 
-use Illuminate\Database\Capsule\Manager as DB;
-
-// Bootup our environment config
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
-$dotenv->load();
-
-// Bootup the database connection
-$db = new DB();
-$db->addConnection([
-	'driver' => 'mysql',
-	'host' => $_ENV['DB_HOST'],
-	'database' => $_ENV['DB_NAME'],
-	'username' => $_ENV['DB_USER'],
-	'password' => $_ENV['DB_PASS'],
-	'charset' => 'utf8',
-	'collation' => 'utf8_unicode_ci',
-	'prefix' => '',
-]);
-$db->setAsGlobal();
-$db->bootEloquent();
+require_once __DIR__ . '/bootstrap.php';
 
 echo '<hr />done';
