46963cebb4622bb2e537020f859940138729522d
Mark Biek <support@janustech.net>
2021-12-22 14:04:00 -0500

    Basic styles with tailwind

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -1,45 +1,54 @@
 @extends('layouts.app')
 
 @section('content')
-<p>Click on an account to impersonate it</p>
 
-<ul>
-@foreach ($accounts as $account)
-	<li>
-		<a href="/?impersonate={{ $account->id }}">{{ $account->name }}</a>
-		{{ $account->is_admin ? '(admin)' : ''}}
-		{{ $account->overdue ? '(overdue)' : ''}}
-	</li>
-@endforeach
-</ul>
+<div class="container mx-auto">
+	<p>Click on an account to impersonate it</p>
+
+	<ul>
+	@foreach ($accounts as $account)
+		<li>
+			<a href="/?impersonate={{ $account->id }}">{{ $account->name }}</a>
+			{{ $account->is_admin ? '(admin)' : ''}}
+			{{ $account->overdue ? '(overdue)' : ''}}
+		</li>
+	@endforeach
+	</ul>
+</div>
 
 @if (!empty($impersonating))
-	<h2>Currently impersonating {{ $impersonating->name }}</h2>
+	<div class="container mx-auto">
+		<h2>Currently impersonating <span class="underline">{{ $impersonating->name }}</span></h2>
 
-	@if ($impersonating->overdue)
-		<p>This account is currently <em>overdue</em>!</p>
-	@endif
-
-	<h2>Checkout</h2>
-	<p>Go to <a href="/checkout?impersonate={{ $impersonating->id }}">checkout</a></p>
-
-	<h2>Product Lists</h2>
-	@foreach ($productLists as $list)
-		<h3>{{ $list->name }}</h3>
-
-		@if (!$impersonating->checkACL('product_list_id', $list->id))
-			<p>This user does not have access to this product list.</p>
+		@if ($impersonating->overdue)
+			<p class="font-bold text-italic text-md">This account is overdue!</p>
 		@endif
+	</div>
 
-		<ul>
-			@foreach ($list->products as $product)
-				<li>
-					<a href="/product?id={{ $product->id }}&impersonate={{ $impersonating->id }}">
-						{{ $product->name }}
-					</a>
-				</li>
-			@endforeach
-		</ul>
-	@endforeach
+	<div class="container mx-auto">
+		<h2>Checkout</h2>
+		<p>Go to <a href="/checkout?impersonate={{ $impersonating->id }}">checkout</a></p>
+	</div>
+
+	<div class="container mx-auto">
+		<h2>Product Lists</h2>
+		@foreach ($productLists as $list)
+			<h3>{{ $list->name }}</h3>
+
+			@if (!$impersonating->checkACL('product_list_id', $list->id))
+				<p class="font-bold text-italic text-sm">This user does not have access to this product list.</p>
+			@endif
+
+			<ul>
+				@foreach ($list->products as $product)
+					<li>
+						<a href="/product?id={{ $product->id }}&impersonate={{ $impersonating->id }}">
+							{{ $product->name }}
+						</a>
+					</li>
+				@endforeach
+			</ul>
+		@endforeach
+	</div>
 @endif
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,11 +1,51 @@
 <html>
     <head>
+        <title>Mark Biek | Zoey Code Test</title>
+        <script src="https://cdn.tailwindcss.com"></script>
+        <style type="text/tailwindcss">
+            @layer base {
+                a {
+                    @apply underline;
+                }
+
+                a:hover {
+                    @apply no-underline;
+                }
+
+                h2 {
+                    @apply text-2xl;
+                }
+
+                h3 {
+                    @apply text-xl mt-2 mb-1;
+                }
+            }
+
+            main {
+                width: 75%;
+                margin: 0 auto;
+            }
+
+            ul {
+                list-style-type: disc;
+                padding-top: 0.5rem;
+                padding-left: 3rem;
+            }
+
+            .container {
+                margin-bottom: 1rem;
+            }
+        </style>
     </head>
     <body>
-        <nav>
-            <h1><a href="/">Home</a></h1>
-        </nav>
+        <main>
+            <nav>
+                <h1 class="text-4xl mb-2"><a href="/">Home</a></h1>
+            </nav>
 
-        @yield('content')
+            <section>
+                @yield('content')
+            </section>
+        </main>
     </body>
 </html>
